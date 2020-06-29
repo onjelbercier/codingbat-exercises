ffd623069c1aa4e6a8191164496fe8c3ed693e0b
Onjel Bercier <onjelbercier@gmail.com>
2020-06-29 19:58:10 -0400

    Created a new array with first and last elements of the orginal array.

---
+++ src/main/java/com/bercier/codingbat/array1/MakeEnds.java
@@ -0,0 +1,21 @@
+package com.bercier.codingbat.array1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MakeEnds {
+
+    /*Given an array of ints, return a new array length 2 containing the first and last elements from the original array.
+    The original array will be length 1 or more.*/
+
+    public List makeEnds(int[] nums) {
+
+        List<Integer>numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        return Arrays.asList(numsList.get(0),numsList.get(nums.length-1));
+
+    }
+
+
+}
