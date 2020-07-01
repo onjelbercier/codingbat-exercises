4c7987b78fed90f2daa08a501c6bef98651301b0
Onjel Bercier <onjelbercier@gmail.com>
2020-07-01 13:38:18 -0400

    Return a boolean for if the list contains a 2 or 3.

---
+++ src/main/java/com/bercier/codingbat/array1/Has23.java
@@ -0,0 +1,25 @@
+package com.bercier.codingbat.array1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Has23 {
+
+    /*Given an int array length 2, return true if it contains a 2 or a 3.*/
+
+    public boolean has23(int[] nums) {
+
+        List<Integer> numsList= Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        if(numsList.get(0) ==2 || numsList.get(0) ==3){
+            return true;
+
+        }else if(numsList.get(1) ==2 || numsList.get(1) ==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
