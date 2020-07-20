33fc178dd95755c93bbea512ef7e5920e7abed46
Onjel Bercier <onjelbercier@gmail.com>
2020-07-20 15:03:10 -0400

    [codingbat-array1]Added makeMiddle class. All tests passed.

--- src/main/java/com/bercier/codingbat/array1/MakeMiddle.java
+++ src/main/java/com/bercier/codingbat/array1/MakeMiddle.java
@@ -1,4 +1,30 @@
 package com.bercier.codingbat.array1;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MakeMiddle {
+
+    /*Given an array of ints of even length,
+    return a new array length 2 containing
+    the middle two elements from the original array.
+    The original array will be length 2 or more.*/
+
+    public List makeMiddle(int[] nums) {
+
+        List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        int middleIndex = (numsList.size() / 2) - 1;
+
+        return Arrays.asList(numsList.get(middleIndex), numsList.get(middleIndex+1));
+
+    }
+
 }
+
+
+
+
+
+
+
