d693f7a7c38a3e8d37bed89c07e18f6e01af8e7c
Onjel Bercier <onjelbercier@gmail.com>
2020-07-23 11:48:11 -0400

    [codingbat-array1]Added SwapEnds.

--- src/main/java/com/bercier/codingbat/array1/SwapEnds.java
+++ src/main/java/com/bercier/codingbat/array1/SwapEnds.java
@@ -1,4 +1,20 @@
 package com.bercier.codingbat.array1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SwapEnds {
+
+    public List swapEnds(int[] nums) {
+
+        List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        Collections.swap(numsList, 0, numsList.size()-1);
+
+        return numsList;
+    }
+
+
 }
