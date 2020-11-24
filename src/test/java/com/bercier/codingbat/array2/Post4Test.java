c97aec1b54318ed0ba501a296633a9f951c08633
Onjel Bercier <onjelbercier@gmail.com>
2020-11-24 16:16:43 -0500

    [codingbat-array2] added Post4Test.

--- src/test/java/com/bercier/codingbat/array2/Post4Test.java
+++ src/test/java/com/bercier/codingbat/array2/Post4Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -14,7 +15,7 @@ public class Post4Test {
     @ParameterizedTest
     @MethodSource("post4TestDataStream")
 
-    public void shouldReturnANewArrayContainingTheElementsFromTheOriginalArrayThatComeAfterTheLastFourInTheOriginalArray(int[] nums, int[] expectedResult){
+    public void shouldReturnANewArrayContainingTheElementsFromTheOriginalArrayThatComeAfterTheLastFourInTheOriginalArray(int[] nums, List<Integer> expectedResult){
 
         Post4 post4 = new Post4();
 
@@ -26,8 +27,16 @@ public class Post4Test {
 
         return Stream.of(
 
-                arguments()
-
+                arguments(new int[]{2, 4, 1, 2}, List.of(1, 2)),
+                arguments(new int[]{4, 1, 4, 2}, List.of(2)),
+                arguments(new int[]{4, 4, 1, 2, 3}, List.of(1, 2, 3)),
+                arguments(new int[]{4, 2}, List.of(2)),
+                arguments(new int[]{4, 4, 3}, List.of(3)),
+                arguments(new int[]{4, 4}, List.of()),
+                arguments(new int[]{4}, List.of()),
+                arguments(new int[]{2, 4, 1, 4, 3, 2}, List.of(3, 2)),
+                arguments(new int[]{4, 1, 4, 2, 2, 2}, List.of(2, 2, 2)),
+                arguments(new int[]{3, 4, 3, 2}, List.of(3, 2))
         );
 
     }
